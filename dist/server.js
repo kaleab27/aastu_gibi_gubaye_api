7226ce94f905615980479a2c808adb68e993c6f1
HaileabT <haileabtesfaye11@gmail.com>
2024-10-18 06:58:56 -0400

    supplementary

--- dist/server.js
+++ dist/server.js
@@ -5,13 +5,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const data_source_1 = require("./data_source");
-const connection = data_source_1.AppDataSource.initialize()
-    .then(() => {
-    app_1.default.listen(3000, () => {
-        console.log('Server is runnng');
+try {
+    const connection = data_source_1.AppDataSource.initialize()
+        .then(() => {
+        app_1.default.listen(3000, () => {
+            console.log('Server is runnng');
+        });
+    })
+        .catch(err => {
+        console.log(err);
     });
-})
-    .catch(err => {
+}
+catch (err) {
     console.log(err);
-});
+    data_source_1.AppDataSource.destroy();
+    data_source_1.AppDataSource.initialize()
+        .then(() => {
+        app_1.default.listen(3000, () => {
+            console.log('Server is runnng');
+        });
+    })
+        .catch(err => {
+        console.log(err);
+    });
+}
 //# sourceMappingURL=server.js.map
