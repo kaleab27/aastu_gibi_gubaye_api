b8fc1faffee94f58eec8a5d033c7f1422061661b
Habtemariam <habteshbaki2014@gmail.com>
2025-02-13 02:52:36 -0800

    remove unnecessary types

--- types/custom.d.ts
+++ types/custom.d.ts
@@ -1,34 +1,8 @@
-import express from 'express';
 import {Confession} from '../models/confessionModel';
 import {Service} from '../models/serviceModel';
-// import { Language } from '../models/languageModel';
 import {Department} from '../models/departmentModel';
-import {LanguageD} from '../domain_entities/language.entity';
 import {Language} from '../models/languageModel';
 import {Request} from 'express';
-
-// declare global {
-//   namespace Express {
-//     interface Request {
-//       student?: {
-//         first_name: string;
-//         last_name: string;
-//         student_id: string;
-//         phone_number: string;
-//         gender: string;
-//         language: string[];
-//         department: string;
-//         baptismal_name?: string;
-//         email?: string;
-//         password?: string;
-//         service?: string[];
-//         role?: string;
-//         current_year?: number;
-//         confession?: string;
-//       };
-//     }
-//   }
-// }
 
 export interface studentReq extends Request {
   student?: {
